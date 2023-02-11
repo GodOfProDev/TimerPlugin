2c723bbc30296ea7bce64df750c81474de317279
GodOfPro <1387ilia@gmail.com>
2023-02-11 13:34:05 +0330

    Initial commit

---
+++ src/main/java/me/godofpro/timerplugin/TimerPlugin.java
@@ -0,0 +1,12 @@
+package me.godofpro.timerplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class TimerPlugin extends JavaPlugin {
+
+  @Override
+  public void onEnable() {
+
+  }
+
+}
